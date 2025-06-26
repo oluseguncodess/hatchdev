2146103f4d41eb7ce7d3278514d51ab58c7b1827
olusegun <olusegunbamgbelu755@gmail.com>
2025-06-26 02:11:14 +0100

    doubly linked list

--- dist/dsa/linkedList/doublyLinkedList.js
+++ dist/dsa/linkedList/doublyLinkedList.js
@@ -74,4 +74,91 @@ class DoublyLinkedList {
         this.length++;
         return this;
     }
+    get(index) {
+        var _a, _b;
+        if (this.length === 0 || index < 0 || index >= this.length)
+            return undefined;
+        if (index === this.length - 1)
+            return (_a = this.tail) !== null && _a !== void 0 ? _a : undefined;
+        if (index === 0)
+            return (_b = this.head) !== null && _b !== void 0 ? _b : undefined;
+        if (index < this.length / 2) {
+            let current = this.head;
+            let counter = 0;
+            while (counter < index) {
+                current = current.next;
+                counter++;
+            }
+            return current !== null && current !== void 0 ? current : undefined;
+        }
+        else {
+            let current = this.tail;
+            let counter = this.length - 1;
+            while (counter > index) {
+                current = current.prev;
+                counter--;
+            }
+            return current !== null && current !== void 0 ? current : undefined;
+        }
+    }
+    print() {
+        if (!this.head)
+            return undefined;
+        let current = this.head;
+        let result = '';
+        while (current) {
+            result += `${current.value} --> `;
+            current = current.next;
+        }
+        return result.slice(0, -5);
+    }
+    insertAt(index, value) {
+        const newNode = new Nodez(value);
+        if (index < 0 || index > this.length)
+            return undefined;
+        if (this.length === 0) {
+            return this.append(value);
+        }
+        if (index === this.length) {
+            return this.append(value);
+        }
+        if (index === 0) {
+            return this.prepend(value);
+        }
+        const prevNode = this.get(index - 1);
+        const nextNode = prevNode.next;
+        prevNode.next = newNode;
+        newNode.prev = prevNode;
+        newNode.next = nextNode;
+        nextNode.prev = newNode;
+        this.length++;
+        return this;
+    }
+    removeAtIndex(index) {
+        if (index < 0 || index >= this.length || this.length === 0)
+            return undefined;
+        if (index === 0) {
+            return this.shift();
+        }
+        if (index === this.length - 1) {
+            return this.pop();
+        }
+        const prevNode = this.get(index - 1);
+        const removedNode = prevNode.next;
+        const nextNode = removedNode.next;
+        prevNode.next = nextNode;
+        nextNode.prev = prevNode;
+        this.length--;
+        return removedNode.value;
+    }
 }
+const list = new DoublyLinkedList(5);
+list.append(6).append(7).append(8).append(9);
+list.prepend(4);
+list.prepend(3);
+list.append(11);
+list.insertAt(7, 10);
+list.insertAt(0, 2);
+list.insertAt(list.length, 12);
+console.log(list.length);
+console.log(list.print());
--- src/dsa/linkedList/doublyLinkedList.ts
+++ src/dsa/linkedList/doublyLinkedList.ts
@@ -20,7 +20,7 @@ class DoublyLinkedList<T> {
     }
   }
 
-  // adds a node to the beginning of list
+  // adds a node at the end of list
   append(val: T): this {
     // create a new node
     const newNode = new Nodez<T>(val)
@@ -47,7 +47,7 @@ class DoublyLinkedList<T> {
     return this;
   }
 
-  // removes the last node
+  // removes the last node at the end of list
   pop(): T | undefined {
     // check to see if the list is empty
     if (this.length === 0) {
@@ -55,7 +55,7 @@ class DoublyLinkedList<T> {
     }
 
     // save the removed node
-    const removedNode = this.tail!
+    const removedNode = this.tail as Nodez<T>
 
     // check if the list has one node
     if (this.length === 1) {
@@ -105,9 +105,10 @@ class DoublyLinkedList<T> {
     return removedNode!.value
   }
 
+  // adds node to the beginning of list
   prepend(value: T): this {
     // check if the list is empty
-    if(this.length === 0) {
+    if (this.length === 0) {
       return this.append(value)
     }
 
@@ -130,4 +131,149 @@ class DoublyLinkedList<T> {
     return this
   }
 
+  // get node at any index
+  get(index: number): Nodez<T> | undefined {
+    // check to see if the list or the index is out of bounds
+    if (this.length === 0 || index < 0 || index >= this.length) return undefined;
+
+    // check if the index is the same as the length of linked list minus one
+    if (index === this.length - 1) return this.tail ?? undefined
+
+    // return the head's value if index is 0
+    if (index === 0) return this.head ?? undefined
+
+    // traverse through list to get specific node
+    if (index < this.length / 2) {
+      let current = this.head
+      let counter = 0;
+      while (counter < index) {
+        current = current!.next
+        counter++
+      }
+      // return node 
+      return current ?? undefined
+    } else {
+      let current = this.tail
+      let counter = this.length - 1
+
+      while (counter > index) {
+        current = current!.prev;
+        counter--
+      }
+      // return node
+      return current ?? undefined
+    }
+  }
+
+  // print list
+  print(): string | undefined {
+    // check if there is a head 
+    if (!this.head) return undefined
+
+    // traverse and append values to a string
+    let current = this.head
+    let result = ''
+
+    while (current) {
+      result += `${current.value} --> `;
+      current = current.next!
+    }
+
+    // remove the last arrow
+    return result.slice(0, -5)
+  }
+
+  // insert at any index
+  insertAt(index: number, value: T): this | undefined {
+    // create a new node
+    const newNode = new Nodez(value)
+
+    // check for invalid index
+    if (index < 0 || index > this.length) return undefined
+
+    // if the list is empty
+    if (this.length === 0) {
+      return this.append(value)
+    }
+
+    // if the index is the same as the list's length
+    if (index === this.length) {
+      return this.append(value)
+    }
+
+    // if index is 0
+    if (index === 0) {
+      return this.prepend(value)
+    }
+
+    // get the previous node of the index
+    const prevNode = this.get(index - 1) as Nodez<T>
+
+    // save it's next node
+    const nextNode = prevNode!.next as Nodez<T>
+
+    // set the prevNode to the newNode and new node's prev property to the prev node
+    prevNode!.next = newNode
+    newNode.prev = prevNode
+
+    // set the newNode's next property to the nextNode and nextNode prev property to newNode
+    newNode.next = nextNode
+    nextNode.prev = newNode
+
+    // increment length
+    this.length++
+
+    // return list
+    return this
+  }
+
+  // remove at any index
+  removeAtIndex(index: number): T | undefined {
+    // check for invalid indexes and empty list
+    if (index < 0 || index >= this.length || this.length === 0) return undefined
+
+    // if index is 0
+    if (index === 0) {
+      return this.shift()
+    }
+
+    // remove at the last index
+    if (index === this.length - 1) {
+      return this.pop()
+    }
+
+    // get the node and
+    const prevNode = this.get(index - 1) as Nodez<T>
+
+    // save the node to be removed
+    const removedNode = prevNode.next as Nodez<T>
+
+    // save the next node 
+    const nextNode = removedNode.next as Nodez<T>
+
+    // set the next property of the prev node the removed node next property
+    prevNode.next = nextNode
+
+    // set the next node prev property to the prev node
+    nextNode.prev = prevNode
+
+    // reduce length
+    this.length--
+
+    // return removed node value
+    return removedNode.value
+
+  }
+
 }
+
+const list = new DoublyLinkedList<number>(5)
+list.append(6).append(7).append(8).append(9)
+list.prepend(4)
+list.prepend(3)
+list.append(11)
+list.insertAt(7, 10)
+list.insertAt(0, 2)
+list.insertAt(list.length, 12)
+console.log(list.length)
+console.log(list.print())
